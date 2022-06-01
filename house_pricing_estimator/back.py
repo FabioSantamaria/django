e12a28321f5fa4a66ad2476c8d835c1cbe4493b1
fabio <fabiosantamaria271@gmail.com>
2022-06-01 08:26:23 +0200

    changes in linear model

--- house_pricing_estimator/back.py
+++ house_pricing_estimator/back.py
@@ -9,13 +9,14 @@ def linear_model(
 		baths_number
 		):
 
+	intercept = 8.351244
 	floor_coef = 0
 	year_coef = 0
 	rooms_coef = -0.067523
 	baths_coef = 0.060102
 
-	y = floor_coef * floor_number + year_coef * year_construction + rooms_coef * rooms_number + baths_coef * baths_number
+	y = intercept + floor_coef * floor_number + year_coef * year_construction + rooms_coef * rooms_number + baths_coef * baths_number
 
 	price = square_meters * np.exp(y)
 
-	return price
+	return np.round(price, 2)
